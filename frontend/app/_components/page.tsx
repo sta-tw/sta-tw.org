97390d3d4fac47baf4cd67d1d01000afbf729c58
Aaron <aaronkao88@gmail.com>
2026-03-05 16:48:30 +0800

    feat: 完成 Info 統計區塊響應式開發

--- frontend/app/_components/page.tsx
+++ frontend/app/_components/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Banner from "./_components/Banner/page";
+import Info from "./_components/Info/page";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Info />
     </>
   );
 }
